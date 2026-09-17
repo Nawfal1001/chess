493ee27ab925dc318f98291003c99176f42c0709
Nawfal1001 <107729100+Nawfal1001@users.noreply.github.com>
2026-09-18 00:32:15 +0200

    chore: add chess core module

---
+++ chess-core/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.library"); id("org.jetbrains.kotlin.android") }
+
+android { namespace = "com.naoufel.decentralchess.chess"; compileSdk = 35
+    defaultConfig { minSdk = 26 }
+}
