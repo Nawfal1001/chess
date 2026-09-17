d61f0b04aa784f33986e1206ddccbbd7835d0d18
Nawfal1001 <107729100+Nawfal1001@users.noreply.github.com>
2026-09-18 00:35:17 +0200

    Add chess core unit test dependency

--- chess-core/build.gradle.kts
+++ chess-core/build.gradle.kts
@@ -3,3 +3,7 @@ plugins { id("com.android.library"); id("org.jetbrains.kotlin.android") }
 android { namespace = "com.naoufel.decentralchess.chess"; compileSdk = 35
     defaultConfig { minSdk = 26 }
 }
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
