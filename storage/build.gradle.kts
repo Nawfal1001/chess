a31e7511cde4df63b4ad9e41161fa1e2f472fe86
Nawfal1001 <107729100+Nawfal1001@users.noreply.github.com>
2026-09-18 00:43:56 +0200

    Add SQLite storage module

---
+++ storage/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.library"); id("org.jetbrains.kotlin.android") }
+
+android {
+    namespace = "com.naoufel.decentralchess.storage"
+    compileSdk = 35
+    defaultConfig { minSdk = 26 }
+}
+
+dependencies {
+    implementation(project(":chess-core"))
+    implementation("androidx.core:core-ktx:1.15.0")
+}
